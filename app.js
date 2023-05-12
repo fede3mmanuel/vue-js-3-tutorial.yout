6de34f2d715ad8b159dd9cedbbf148c7e5772768
Emmanuel Sanchez <fede@fede.website>
2023-05-12 12:25:57 -0600

    v-for

--- app.js
+++ app.js
@@ -2,26 +2,16 @@ const app = Vue.createApp({
     data() {
         return {
             showBooks: true,
-            title: 'The final empire',
-            author: 'Brandom',
-            age: 45,
-            x: 0,
-            y: 0
+            books: [
+                {title: 'name of the wind', author: 'patrick rothfuss'},
+                {title: 'the way of kings', author: 'brandom sanderson'},
+                {title: 'the final empire', author: 'brandom sanderson'},
+            ],
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks;
-        },
-        handleEvent(e, data) {
-            console.log(e, e.type);
-            if (data) {
-                console.log(data);
-            }
-        },
-        handleMousemove(e) {
-            this.x = e.offsetX;
-            this.y = e.offsetY;
         }
     }
 })
