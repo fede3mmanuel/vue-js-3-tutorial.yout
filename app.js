d7e110b5354601fdfa075e4cd95a48dabd3450a1
Emmanuel Sanchez <fede@fede.website>
2023-05-15 12:46:01 -0600

    class change

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const app = Vue.createApp({
             url: 'https://google.com',
             showBooks: true,
             books: [
-                {title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
-                {title: 'the way of kings', author: 'brandom sanderson', img: 'assets/2.jpg', isFav: false },
-                {title: 'the final empire', author: 'brandom sanderson', img: 'assets/3.jpg', isFav: true },
+                {title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true, id: 0 },
+                {title: 'the way of kings', author: 'brandom sanderson', img: 'assets/2.jpg', isFav: false, id: 1 },
+                {title: 'the final empire', author: 'brandom sanderson', img: 'assets/3.jpg', isFav: true, id: 2 },
             ],
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks;
+        },
+        toggleFav(idOfTheBook) {
+            this.$data.books[idOfTheBook].isFav = !this.$data.books[idOfTheBook].isFav;
         }
     }
 })
